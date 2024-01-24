a0e665c0e4ece15374ac2b03fc2390e73a4ed44d
OREO <134676738+OREO206@users.noreply.github.com>
2024-01-24 13:40:10 +0900

    Add files via upload

---
+++ src/ch01/P.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class P {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 1; i <=10; i++) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		
+	}
+
+}
